4aca6338922cff6742fa22b7b8514337d14e64c7
VarunParab <90065369+VarunParab@users.noreply.github.com>
2023-06-19 00:51:13 +0530

    Update awsConfig.js

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk';
 AWS.config.update({
-    accessKeyId: 'AKIAX3GW77ZAVZ3S64W2',
-    secretAccessKey: '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',
+    accessKeyId: '-',
+    secretAccessKey: '-',
     region: 'ap-south-1',
   });
   export default AWS;
